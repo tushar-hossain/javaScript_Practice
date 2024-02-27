1fd3210eefa0611bc25a858eda9d7204273af46e
Tushar Hossain <tusharhossain455@gmail.com>
2024-02-27 23:51:25 +0600

    basic number

---
+++ Chapter 1/5 JS Basic Numbers/index.js
@@ -0,0 +1,19 @@
+// Basic Numbers in javaScript
+
+var n = 14125;
+var f = 3.1416;
+var nn = Number(45);
+
+console.log(nn);
+console.log(Number.parseFloat(nn));
+console.log(Number.parseInt(f));
+
+var nnn = nn.toString();
+console.log(nnn);
+
+console.log(Number.MAX_SAFE_INTEGER);
+
+console.log(Number.MIN_SAFE_INTEGER);
+
+console.log(1 / 0);
+console.log("abc" * 10);
