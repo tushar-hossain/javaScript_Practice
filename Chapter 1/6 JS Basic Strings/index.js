11768413b215b0be11ff7e2cb6f3122b9dfa7be6
Tushar Hossain <tusharhossain455@gmail.com>
2024-02-27 23:59:44 +0600

    String

---
+++ Chapter 1/6 JS Basic Strings/index.js
@@ -0,0 +1,15 @@
+// JS Basic Strings
+
+// String literals
+
+var str = 'String';
+var str2 = "String";
+var str3 = `String`;
+console.log(str, str2, str3);
+
+// Constructor
+
+var str4 = String('string');
+var str5 = String(156);
+var str6 = String(3.14);
+console.log(str4, str5,str6);
