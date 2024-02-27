f2ed3d6f2b7a28482dda6a9b29f47ce00424f05b
Tushar Hossain <tusharhossain455@gmail.com>
2024-02-27 16:44:11 +0600

    variable declaration

---
+++ Chapter 1/2 What is Variables/index.js
@@ -0,0 +1,9 @@
+// What is Variables
+
+var name = "Tushar Hossain";
+var age = 23;
+
+console.log(name + " Knows JavaScript.");
+console.log("His age is only " + age);
+console.log(name + "is Creating a JavaScript course for all.");
+console.log("But his age is  only " + age);
