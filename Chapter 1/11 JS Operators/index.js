7e5f969e698559f689ebbcb24b10e688d6446216
Tushar Hossain <tusharhossain455@gmail.com>
2024-02-28 02:13:14 +0600

    operator

---
+++ Chapter 1/11 JS Operators/index.js
@@ -0,0 +1,35 @@
+// JS Operators
+
+// 1. Arithmetic operator => +, -, *, /, %, ++, --
+var a = 10;
+var b = 3;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+console.log(a++);
+console.log(a);
+console.log(++a);
+console.log(a--);
+console.log(a);
+console.log(--a);
+
+// 2. Assignment operator => =, +=, -=, *=, /=, %=
+
+console.log((a += b));
+console.log((a -= b));
+console.log((a *= b));
+console.log((a /= b));
+
+// 3. Comparison operator => ==, !=, >, <, >=, <=, ===, !==
+console.log(a != b);
+
+// 4. Bitwase operator => &, |, ~, ^, <<, >>
+
+// 5. Logical operator => AND &&, OR ||, NOT !
+console.log(a && b);
+
+// 6. typeof operator
+console.log(typeof a);
