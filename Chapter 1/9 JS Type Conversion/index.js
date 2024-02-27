cac4c83ee6732c346bad53bed0e9947718d17a0a
Tushar Hossain <tusharhossain455@gmail.com>
2024-02-28 01:25:33 +0600

    type conversion

---
+++ Chapter 1/9 JS Type Conversion/index.js
@@ -0,0 +1,20 @@
+// JS Type Conversion
+
+var str = "1000";
+var n = 10;
+
+console.log(str * n);
+console.log(str + n);
+
+console.log(Number.parseInt(str));
+console.log(n.toString());
+
+console.log(Number.Infinity);
+console.log(Boolean(Infinity));
+console.log(Boolean(-Infinity));
+console.log(String(Infinity));
+
+// Falsy values
+/*
+ '', 0, NaN, null, undefine
+ */
