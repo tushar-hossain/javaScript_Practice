adcbfc68775fd36145eeb5011473cfe717f200ad
Tushar Hossain <tusharhossain455@gmail.com>
2024-02-27 17:26:07 +0600

    reserved word and variable declaration

---
+++ Chapter 1/3 JS Reserved Words/index.js
@@ -0,0 +1,15 @@
+// JS Reserved Words
+
+// abstract, else, if, await, switch, case, default, this, throw, true, false, finally, try, catch, var, while, do, float, new, continue, for,delete, in, short,export, import etc...
+
+// let else = 'something'
+// console.log(else);
+
+// Variable declaration Rules
+
+// 1. Variable names must start with a letter, an underscore (_) or a dollar sign ($).
+// 2. Variable names cannot contain spaces.
+// 3. Variables cannot be the same as reserved keywords such as if or const.
+// 4. By convention, JavaScript variable names are written in camelCase.
+// 5. Variables should be given descriptive names that indicate their content and usage.
+// 6. As JavaScript variables do not have set types, it can be useful to include an indication of the type in the name.
