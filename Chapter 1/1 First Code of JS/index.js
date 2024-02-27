02b7202363dddef967f7fc3ec43282b953997071
Tushar Hossain <tusharhossain455@gmail.com>
2024-02-27 16:25:41 +0600

    string and number data type print

---
+++ Chapter 1/1 First Code of JS/index.js
@@ -0,0 +1,10 @@
+// What is Programming?
+// Programming is a set of instruction which our computer can read, understand and produce and output.
+
+console.log("Hello world!");
+console.log(45);
+console.log(65.789);
+
+console.log("My favorite number: " + 9);
+console.log(9 + 9);
+console.log("9" + 9);
