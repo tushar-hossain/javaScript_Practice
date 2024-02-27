82deb3f69c9f1729d9c6918440320e649abc706b
Tushar Hossain <tusharhossain455@gmail.com>
2024-02-27 17:38:15 +0600

    data types

---
+++ Chapter 1/4 JS Data Types/index.js
@@ -0,0 +1,13 @@
+// JS Data Types
+
+// JavaScript two data types
+
+/*
+
+1. Primitive data type
+=> Number, String, Null, Undifine, Boolean
+
+2. Object data type
+=> Array, Object, Function
+
+ */
