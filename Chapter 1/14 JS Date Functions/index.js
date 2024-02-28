f743aa1580d3a3903d5134a4bfe22fc419805d3a
Tushar Hossain <tusharhossain455@gmail.com>
2024-02-28 11:29:22 +0600

    date function

---
+++ Chapter 1/14 JS Date Functions/index.js
@@ -0,0 +1,16 @@
+// JS Date Functions
+
+var date = new Date();
+
+console.log(date);
+console.log(date.toLocaleDateString());
+console.log(date.toLocaleTimeString());
+console.log(date.toString());
+
+console.log(date.getFullYear());
+console.log(date.getDate());
+console.log(date.getHours());
+console.log(date.getMilliseconds());
+console.log(date.getMinutes());
+console.log(date.getSeconds());
+console.log(date.getTime());
