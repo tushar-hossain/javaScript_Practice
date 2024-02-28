898ed125cee1ab9d73c4fe8097346453bb09b116
Tushar Hossain <tusharhossain455@gmail.com>
2024-02-28 18:02:01 +0600

    while loop

--- Chapter 3/25 JS While Loop/index.js
+++ Chapter 3/25 JS While Loop/index.js
@@ -14,3 +14,15 @@ while (x <= 100) {
   x++;
 }
 
+var isRunning = true;
+
+while (isRunning) {
+  var rand = Math.floor(Math.random() * 10 + 1);
+  if (rand === 9) {
+    console.log("Winner winner chicken dinner");
+    // break;
+    isRunning = false;
+  } else {
+    console.log("You have got " + rand);
+  }
+}
