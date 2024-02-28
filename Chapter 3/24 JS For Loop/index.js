620888324b69a1a0bb872a72c8a4864bcfa5cadc
Tushar Hossain <tusharhossain455@gmail.com>
2024-02-28 17:30:30 +0600

    for loop

---
+++ Chapter 3/24 JS For Loop/index.js
@@ -0,0 +1,24 @@
+// JS For Loop
+
+for (var i = 0; i < 10; i++) {
+  console.log("Tushar Hossain");
+}
+
+//  1 to 100 number print for loop
+for (var i = 1; i <= 100; i++) {
+  console.log(i);
+}
+
+for (var i = 0; i < 100; i++) {
+  console.log(i + 1);
+}
+
+// Odd number
+for (var i = 1; i < 100; i += 2) {
+  console.log(i);
+}
+
+// // Even number
+for (var i = 2; i < 100; i += 2) {
+  console.log(i);
+}
