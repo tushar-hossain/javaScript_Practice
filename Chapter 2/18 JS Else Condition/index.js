a50a10c86212b003b3d55ff133c8f0b351314453
Tushar Hossain <tusharhossain455@gmail.com>
2024-02-28 12:31:15 +0600

    else condition

---
+++ Chapter 2/18 JS Else Condition/index.js
@@ -0,0 +1,26 @@
+// JS Else Condition
+
+var a = 10;
+var b = 20;
+
+if (a > b) {
+  console.log(a + " is greter than " + b);
+} else {
+  console.log(b + " is greter than " + a);
+}
+
+var n = 5;
+
+if (n % 2 === 0) {
+  console.log(n + " is Even number");
+} else {
+  console.log(n + " is Odd number");
+}
+
+var mark = 33;
+
+if (mark === 33) {
+  console.log("You have Pass");
+} else {
+  console.log("You have Faield");
+}
