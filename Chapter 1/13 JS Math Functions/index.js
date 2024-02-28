90f79cf65305d85d9335bd59a9b2b2f848b74df5
Tushar Hossain <tusharhossain455@gmail.com>
2024-02-28 11:18:01 +0600

    math function

---
+++ Chapter 1/13 JS Math Functions/index.js
@@ -0,0 +1,14 @@
+// JS Math Functions
+
+console.log(Math.E);
+console.log(Math.PI);
+console.log(Math.sqrt(16));
+console.log(Math.abs(5.55453));
+console.log(Math.pow(2, 3));
+console.log(Math.floor(4.52));
+console.log(Math.ceil(5.6));
+console.log(Math.round(5.124));
+console.log(Math.floor(Math.random() * 10 + 1));
+console.log(Math.max(500, 400, 600));
+console.log(Math.min(200, 400, 50));
+console.log(Math.exp(5));
