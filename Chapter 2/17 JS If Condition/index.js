17784533e3bec8789ed97376cb73096ee2bd3f3e
Tushar Hossain <tusharhossain455@gmail.com>
2024-02-28 12:17:24 +0600

    if condition

---
+++ Chapter 2/17 JS If Condition/index.js
@@ -0,0 +1,22 @@
+// JS If Condition
+
+var a = 10;
+var b = 20;
+
+if (a > b) {
+  console.log("A is greter than B");
+}
+
+if (a < b) {
+  console.log("B is greter than A");
+}
+
+var n = 5;
+
+if (n % 2 === 0) {
+  console.log("N is a Even number");
+}
+
+if (n % 2 === 1) {
+  console.log("N is Odd number");
+}
