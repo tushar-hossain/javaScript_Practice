ebce98d17440c7303fdf54de349ea48f800ee026
Tushar Hossain <tusharhossain455@gmail.com>
2024-02-29 18:43:02 +0600

    escape notation

---
+++ Chapter 4/32 JS Escape Notation/index.js
@@ -0,0 +1,15 @@
+// JS Escape Notation
+
+var str = 'This is a "\String\"';
+console.log(str);
+var str2 = 'This is a \'String\'';
+console.log(str2);
+
+var str3 = "This is a \nString";
+console.log(str3);
+
+var str4 = "This is a \\String";
+console.log(str4);
+
+var str5 = "This is a \tString";
+console.log(str5);
