2da689f95b804a06a2d036b711ed8b4ff2b3d82e
Tushar Hossain <tusharhossain455@gmail.com>
2024-02-29 18:35:01 +0600

    string literal vs constructor

---
+++ Chapter 4/31 String Literal vs Constructor/index.js
@@ -0,0 +1,16 @@
+// String Literal vs Constructor
+
+//string literals
+var str = "string";
+console.log(str);
+
+// constructor
+var str2 = String("something");
+console.log(str2);
+
+var n = 10;
+var str3 = String(n);
+var str4 = n.toString(n);
+
+console.log(str3);
+console.log(str4);
