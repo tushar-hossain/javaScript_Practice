78d6e7d54758358bd728e19235e6b08d5a2a75aa
Tushar Hossain <tusharhossain455@gmail.com>
2024-02-29 19:30:25 +0600

    string comparison

---
+++ Chapter 4/33 JS String Comparison/index.js
@@ -0,0 +1,14 @@
+// JS String Comparison
+
+var a = "abc";
+var b = "bcd";
+
+console.log(a === b);
+console.log(a > b);
+console.log(a < b);
+
+// JavaScript string comparison lexicographically system
+
+var c = "001";
+var d = 1;
+console.log(c == d);
