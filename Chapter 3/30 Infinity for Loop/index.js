a26bbdf074f17abc4d8ff57c62c34b26d765ed49
Tushar Hossain <tusharhossain455@gmail.com>
2024-02-29 18:21:45 +0600

    infinity for loop

---
+++ Chapter 3/30 Infinity for Loop/index.js
@@ -0,0 +1,11 @@
+// Infinity for Loop
+
+for (;;) {
+  var rand = Math.floor(Math.random() * 10 + 1);
+  if (rand === 9) {
+    console.log("Winner winner chicken dinner.");
+    break;
+  } else {
+    console.log("You have got " + rand);
+  }
+}
