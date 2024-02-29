ebd893b3584f0ea4a5eb9b0384f95708d4f94921
Tushar Hossain <tusharhossain455@gmail.com>
2024-02-29 18:00:14 +0600

    continue statement

---
+++ Chapter 3/29 JS Continue Statement/index.js
@@ -0,0 +1,17 @@
+// JS Continue Statement
+
+for (var i = 1; i <= 100; i++) {
+  if (i === 9) {
+    continue;
+  } else {
+    console.log(i);
+  }
+}
+
+for (var i = 0; i <= 10; i++) {
+  if (i === 3 || i === 7) {
+    continue;
+  } else {
+    console.log(i);
+  }
+}
