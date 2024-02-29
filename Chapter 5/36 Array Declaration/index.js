6566aa320615d8f6ee81b2fac080670a976be742
Tushar Hossain <tusharhossain455@gmail.com>
2024-02-29 22:15:51 +0600

    array declaration

---
+++ Chapter 5/36 Array Declaration/index.js
@@ -0,0 +1,15 @@
+// Array Declaration
+
+var arr = [1, 2, 3, 4, 5];
+
+arr[5] = 6;
+arr[3] = 22;
+arr[10] = 11;
+
+console.log(arr[8]);
+console.log(arr[0]);
+console.log(arr[4]);
+console.log(arr);
+
+var arr2 = Array(1, 2, 3, 4);
+console.log(arr2);
