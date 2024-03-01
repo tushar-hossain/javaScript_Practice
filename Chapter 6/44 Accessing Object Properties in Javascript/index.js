230c2dfe464eaa7a7ee11685d91a2ac3b70259ea
Tushar Hossain <tusharhossain455@gmail.com>
2024-03-01 16:19:18 +0600

    accessing object properties

---
+++ Chapter 6/44 Accessing Object Properties in Javascript/index.js
@@ -0,0 +1,21 @@
+// Accessing Object Properties in Javascript
+
+var obj = {
+  x: 10,
+  y: 20,
+  z: 30,
+};
+
+// 1st wayaz dot Notation
+console.log(obj);
+console.log(obj.x);
+console.log(obj.y);
+console.log(obj.z);
+
+// 2nd wayaz Array Notation
+console.log(obj["x"]);
+console.log(obj["y"]);
+console.log(obj["z"]);
+
+var show = "x";
+console.log(obj[show]);
