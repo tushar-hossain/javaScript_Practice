afc2c0d4529999a4a9aa065aee854cf859cf163b
Tushar Hossain <tusharhossain455@gmail.com>
2024-03-01 19:50:50 +0600

    defining a function

---
+++ Chapter 7/50 Defining a Function in Javascript/index.js
@@ -0,0 +1,13 @@
+// Defining a Function in Javascript
+
+function sum() {
+  var a = 10;
+  var b = 20;
+  console.log(a + b);
+}
+
+function sub() {
+  var a = 50;
+  var b = 20;
+  console.log(a - b);
+}
