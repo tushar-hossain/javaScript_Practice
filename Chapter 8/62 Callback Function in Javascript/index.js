50bc43f3c085cd32f65f2caef69ed8f29c494662
Tushar Hossain <tusharhossain455@gmail.com>
2024-03-04 16:53:36 +0600

    callback function

---
+++ Chapter 8/62 Callback Function in Javascript/index.js
@@ -0,0 +1,33 @@
+// Callback Function in Javascript
+
+function sample(a, b, cb) {
+  var c = a + b;
+  var d = a - b;
+  var result = cb(c, d);
+  return result;
+}
+
+function sum(a, b) {
+  return a + b;
+}
+
+var result = sample(5, 8, sum);
+console.log(result);
+
+var result2 = sample(5, 8, function (c, d) {
+  return c - d;
+});
+console.log(result2);
+
+var result3 = sample(5, 8, function (c, d) {
+  return c * d;
+});
+console.log(result3);
+
+var result4 = sample(5, 8, function (c, d) {
+  return c / d;
+});
+
+// console.log(result4);
+console.log(result4.toPrecision(3));
+console.log(result4.toFixed(2));
