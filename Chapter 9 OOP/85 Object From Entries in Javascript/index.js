f05460c2bc52940f07a9e255c287ab909c967847
Tushar Hossain <tusharhossain455@gmail.com>
2024-03-08 22:01:02 +0600

    object from entrise

---
+++ Chapter 9 OOP/85 Object From Entries in Javascript/index.js
@@ -0,0 +1,16 @@
+// Object From Entries in Javascript
+
+let obj = {
+  a: 10,
+  b: 20,
+};
+
+console.log(Object.entries(obj));
+
+let arr = [
+  ["name", "Tushar"],
+  ["email", "tusharhossain455@gmail.com"],
+  ["address", "Dhaka"],
+];
+
+console.log(Object.fromEntries(arr));
