f33d5009bb30f5ec62e5b837178b9397415e57fa
Tushar Hossain <tusharhossain455@gmail.com>
2024-04-26 21:06:34 +0600

    asynchronous programming with js

---
+++ Chapter 13/116 Asynchronous Programming is Action in Javascript/index.js
@@ -0,0 +1,17 @@
+// Asynchronous Programming is Action in Javascript
+
+console.log("I am line one");
+
+setTimeout(() => {
+  console.log("I am line two");
+}, 3000);
+
+setTimeout(() => {
+  console.log("I am line three");
+}, 0);
+
+console.log("I am line four");
+
+setTimeout(() => {
+  console.log("I am line five");
+}, 5000);
