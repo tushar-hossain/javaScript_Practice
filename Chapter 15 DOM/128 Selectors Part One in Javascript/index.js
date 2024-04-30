62e1b287e12a9aead1d256641fdc139219058316
Tushar Hossain <tusharhossain455@gmail.com>
2024-05-01 00:16:54 +0600

    selectors part one in js

---
+++ Chapter 15 DOM/128 Selectors Part One in Javascript/index.js
@@ -0,0 +1,14 @@
+// Selectors Part One in Javascript
+
+let title = document.getElementById("title");
+// console.log(title);
+
+let paragraph = document.getElementsByClassName("lead");
+
+// console.log(paragraph);
+
+let list = document.getElementsByTagName("li");
+console.log(list);
+
+let names = document.getElementsByName(["list-item"]);
+console.log(names);
