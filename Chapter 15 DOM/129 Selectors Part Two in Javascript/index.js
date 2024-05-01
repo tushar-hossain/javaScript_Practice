62a4ed12671f33bf549a78dd28ca009246bce879
Tushar Hossain <tusharhossain455@gmail.com>
2024-05-01 12:33:02 +0600

    selector part two in js

---
+++ Chapter 15 DOM/129 Selectors Part Two in Javascript/index.js
@@ -0,0 +1,11 @@
+// Selectors Part Two in Javascript
+
+let heading = document.querySelector("#heading");
+console.log(heading);
+
+let paragraph = document.querySelectorAll(".lead");
+
+console.log(paragraph);
+
+let list = document.querySelectorAll("li");
+console.log(list);
