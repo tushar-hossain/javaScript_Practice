49619516035b8ed8dd918e480e770416546f2d1f
Tushar_Hossain <tusharhossain455@gmail.com>
2024-05-07 23:47:49 +0600

    form handling dom elements in js

---
+++ Chapter 15 DOM/144  Form Handling in DOM in Javascript/index.js
@@ -0,0 +1,16 @@
+//  Form Handling in DOM in Javascript
+
+let form = document.getElementById("form");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  let formData = {};
+  [...this.elements].forEach((el) => {
+    if (el.type !== "submit") {
+      formData[el.name] = el.value;
+    }
+  });
+  console.log(formData);
+  this.reset();
+});
