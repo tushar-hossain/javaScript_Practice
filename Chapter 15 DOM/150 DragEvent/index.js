51a5d05fd6ef3056fb9a30b171d702702f3a5703
Tushar_Hossain <tusharhossain455@gmail.com>
2024-05-12 00:46:22 +0600

    two box drag event in js

--- Chapter 15 DOM/150 DragEvent/index.js
+++ Chapter 15 DOM/150 DragEvent/index.js
@@ -1,7 +1,25 @@
 // DragEvent
 
-let div = document.querySelector("div");
-let p = document.querySelector("p");
+// let div = document.querySelector("div");
+// let p = document.querySelector("p");
+
+// p.addEventListener("dragstart", function (e) {
+//   e.dataTransfer.setData("text", e.target.id);
+// });
+
+// div.addEventListener("dragover", function (e) {
+//   e.preventDefault();
+// });
+
+// div.addEventListener("drop", function (e) {
+//   let id = e.dataTransfer.getData("text");
+//   div.appendChild(document.getElementById(id));
+//   e.preventDefault();
+// });
+
+let div = document.getElementById("dragEvent");
+
+let p = document.getElementById("para");
 
 p.addEventListener("dragstart", function (e) {
   e.dataTransfer.setData("text", e.target.id);
@@ -12,7 +30,24 @@ div.addEventListener("dragover", function (e) {
 });
 
 div.addEventListener("drop", function (e) {
-  let id = e.dataTransfer.getData("text");
-  div.appendChild(document.getElementById(id));
+  let data = e.dataTransfer.getData("text");
+  this.appendChild(document.getElementById(data));
+  e.preventDefault();
+});
+
+// Two box dragable event
+let para2 = document.getElementById("para2");
+
+div.addEventListener("dragstart", function (e) {
+  e.dataTransfer.setData("text", e.target.id);
+});
+
+para2.addEventListener("dragover", function (e) {
+  e.preventDefault();
+});
+
+para2.addEventListener("drop", function (e) {
+  let Data = e.dataTransfer.getData("text");
+  this.appendChild(document.getElementById(Data));
   e.preventDefault();
 });
